6c4c6d292fcc207ba839fe7b4aba4ffb2a6f947f
Philip Meier <github.pmeier@posteo.de>
2020-07-08 15:13:13 +0200

    update cookiecutter.json (#48)
    * update cookiecutter.json
    
    * fix pre gen hook

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,12 +1,5 @@
-import re
 import sys
 from keyword import iskeyword
-
-
-def verify(expr: bool, msg: str) -> None:
-    if not expr:
-        print(f"{msg.strip()} Aborting!")
-        sys.exit(1)
 
 
 def verify_pkg_name(pkg_name: str = "{{cookiecutter.package}}") -> None:
@@ -15,29 +8,13 @@ def verify_pkg_name(pkg_name: str = "{{cookiecutter.package}}") -> None:
     def is_valid_variable_name(name: str) -> bool:
         return name.isidentifier() and not iskeyword(name)
 
-    verify(is_valid_variable_name(pkg_name), f"Package name {pkg_name} is not a valid.")
-
-
-def verify_base_version(base_version: str = "{{cookiecutter.base_version}}") -> None:
-    # Copied from
-    # https://www.python.org/dev/peps/pep-0440/#appendix-b-parsing-version-strings-with-regular-expressions
-    def is_canonical(version: str) -> bool:
-        return (
-            re.match(
-                r"^([1-9][0-9]*!)?(0|[1-9][0-9]*)(\.(0|[1-9][0-9]*))*((a|b|rc)(0|[1-9][0-9]*))?(\.post(0|[1-9][0-9]*))?(\.dev(0|[1-9][0-9]*))?$",
-                version,
-            )
-            is not None
-        )
-
-    verify(
-        is_canonical(base_version), f"Base version {base_version} is not canonical.",
-    )
+    if not is_valid_variable_name(pkg_name):
+        print(f"Package name {pkg_name} is not a valid. Aborting!")
+        sys.exit(1)
 
 
 def main() -> None:
     verify_pkg_name()
-    verify_base_version()
 
 
 if __name__ == "__main__":
